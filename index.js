f5f83c535219c72113515a4f115686e10f7ffe90
dscottyoung2022 <131805518+dscottyoung2022@users.noreply.github.com>
2024-07-17 10:59:58 -0400

    Added basic form handling and Typescript

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Hello Daniella");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "The poem will be displayed here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
